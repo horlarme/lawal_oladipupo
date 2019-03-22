52a1e1e48678d533e7015c5a766bf5b35ab65166
Lawal Oladipupo <lawaloladipupo@outlook.com>
2019-03-22 15:24:11 +0100

    Added summary as text with image to basic profile view

--- lib/screens/TabViews/BasicProfileTabView.dart
+++ lib/screens/TabViews/BasicProfileTabView.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lawal_oladipupo/widgets/CustomCircularLoading.dart';
 import 'package:lawal_oladipupo/widgets/ProfileRow.dart';
+import 'package:drop_cap_text/drop_cap_text.dart';
 
 class BasicProfileTabView extends StatefulWidget {
   @override
@@ -13,13 +14,30 @@ class _BasicProfileTabView extends State<BasicProfileTabView> {
   List<Widget> _data = [
     Padding(
       padding: EdgeInsets.all(20),
-      child: ClipOval(
-          child: Image.asset(
-            'assets/images/lawal.jpeg',
-            width: 220,
-            height: 220,
-            fit: BoxFit.cover,
-          )),
+      child: DropCapText(
+        "Lorem Ipsum is simply dummy text of the printing and "
+            "typesetting industry. Lorem Ipsum has been the industry's standard "
+            "dummy text ever since the 1500s, when an unknown printer took a galley "
+            "of type and scrambled it to make a type specimen book. It has survived "
+            "not only five centuries, but also the leap into electronic typesetting,"
+            " remaining essentially unchanged. It was popularised in the 1960s with "
+            "the release of Letraset sheets containing Lorem Ipsum passages, and more"
+            "recently with desktop publishing software like Aldus PageMaker including "
+            "versions of Lorem Ipsum.",
+        textAlign: TextAlign.justify,
+        dropCap: DropCap(
+            width: 100,
+            height: 100,
+            child: Padding(
+                padding: EdgeInsets.all(5),
+                child: ClipOval(
+                    child: Image.asset(
+                  'assets/images/lawal.jpeg',
+                  width: 100,
+                  height: 100,
+                  fit: BoxFit.cover,
+                )))),
+      ),
     ),
     ProfileRow("Full Name", "Lawal Oladipupo"),
     ProfileRow("Gender", "Male"),
