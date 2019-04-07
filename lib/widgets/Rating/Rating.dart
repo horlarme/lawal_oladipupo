8187a4ba6d1efbc77331a80029c3a5c8de039c10
Lawal Oladipupo <lawaloladipupo@outlook.com>
2019-04-07 17:34:04 +0200

    Created Rating Summary Widget

---
+++ lib/widgets/Rating/Rating.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:lawal_oladipupo/config/Colors.dart';
+
+class Rating extends StatelessWidget {
+  final int ratings;
+
+  Rating({Key key, this.ratings}) : super(key: key);
+
+  //create starts by the number of ratings provided
+  getStars() {
+    List<Widget> stars = [];
+
+    //if rating doesn't have any value
+    if (this.ratings != null) {
+      for (var i = 1; i <= this.ratings; i++) {
+        stars.add(Icon(Icons.stars));
+      }
+    }
+
+    return stars;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: MediaQuery.of(context).size.width / 2,
+        decoration: BoxDecoration(
+            border: Border.all(width: 2),
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        padding: EdgeInsets.all(15),
+        margin: EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+                padding: EdgeInsets.all(5),
+                child: Text(
+                  "ratings".toUpperCase(),
+                  style: TextStyle(
+                      color: textColor,
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold),
+                )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  this.ratings != null ? this.ratings.toString() : "0",
+                  style: TextStyle(
+                      color: ascentColor,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "STARs".toUpperCase(),
+                  style: TextStyle(color: textColor, fontSize: 13),
+                )
+              ],
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: this.getStars()),
+            Container(
+              margin: EdgeInsets.only(top: 5),
+              decoration: BoxDecoration(
+                border: Border(top: BorderSide(width: 2)),
+              ),
+              child: RaisedButton(
+                onPressed: () {
+                  print("onTap called.");
+                },
+                color: Colors.black,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.rate_review, color: Colors.white),
+                    Text(
+                      " Rate User",
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+}
